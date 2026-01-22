93bb45de3a4ecfd2124ab9603b5fcc5f917efd11
tmasj <253758115+tmasj@users.noreply.github.com>
2026-01-22 14:52:13 -0600

    new vertices, with indices in reverse order of depth, so they are drawn in the wrong order, exposing the usefulness of a depth buffer.

--- src/main.rs
+++ src/main.rs
@@ -1015,7 +1015,7 @@ fn main() {
 #[repr(C)]
 #[derive(Copy, Clone, Debug)]
 struct Vertex {
-    position: [f32; 2],
+    position: [f32; 3],
     color: [f32; 3],
     // Could add texture coords, ambient occlusion, etc.
 }
@@ -1034,7 +1034,7 @@ impl Vertex {
         let first = vk::VertexInputAttributeDescription::default()
             .binding(0)
             .location(0)
-            .format(vk::Format::R32G32_SFLOAT)
+            .format(vk::Format::R32G32B32_SFLOAT)
             .offset(std::mem::offset_of!(Vertex,position) as u32);
         let second = vk::VertexInputAttributeDescription::default()
             .binding(0)
@@ -1048,17 +1048,17 @@ impl Vertex {
 
 fn get_triangle_geometry() -> Vec<Vertex> {
     vec![
-        Vertex { position: [0.0, -0.5], color: [1.0, 0.0, 0.2] },
-        Vertex { position: [0.5, 0.5], color: [0.1, 0.5, 1.0] },
-        Vertex { position: [-0.5, 0.5], color: [0.7, 0.5, 0.0] },
+        Vertex { position: [0.0, -0.5, 0.0], color: [1.0, 0.0, 0.2] },
+        Vertex { position: [0.5, 0.5, 0.0], color: [0.1, 0.5, 1.0] },
+        Vertex { position: [-0.5, 0.5, 0.0], color: [0.7, 0.5, 0.0] },
     ]
 }
 
 fn get_triangle_geometry2() -> Vec<Vertex> {
     vec![
-        Vertex { position: [0.5, -0.5], color: [1.0, 0.0, 0.2] },
-        Vertex { position: [0.0, 0.5], color: [0.1, 0.5, 1.0] },
-        Vertex { position: [0.5, 0.5], color: [0.7, 0.5, 0.0] },
+        Vertex { position: [0.5, -0.5, 0.0], color: [1.0, 0.0, 0.2] },
+        Vertex { position: [0.0, 0.5, 0.0], color: [0.1, 0.5, 1.0] },
+        Vertex { position: [0.5, 0.5, 0.0], color: [0.7, 0.5, 0.0] },
     ]
 }
 
@@ -1066,15 +1066,20 @@ type GeometryDataIndex = u16; // Can be u16 or u32
 
 fn triangle_vertices_indexed() -> Vec<Vertex> {
     vec![
-        Vertex { position: [-0.5, -0.5], color: [1.0, 0.0, 0.0] },
-        Vertex { position: [0.5, -0.5], color: [0.0, 1.0, 0.0] },
-        Vertex { position: [0.5, 0.5], color: [0.0, 0.0, 1.0] },
-        Vertex { position: [-0.5, 0.5], color: [1.0, 1.0, 1.0] },
+        Vertex { position: [-0.5, -0.5, 0.0], color: [1.0, 0.0, 0.0] },
+        Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+        Vertex { position: [0.5, 0.5, 0.0], color: [0.0, 0.0, 1.0] },
+        Vertex { position: [-0.5, 0.5, 0.0], color: [1.0, 1.0, 1.0] },
+        Vertex { position: [-0.5, -0.5, -0.5], color: [1.0, 0.0, 0.0] },
+        Vertex { position: [0.5, -0.5, -0.5], color: [0.0, 1.0, 0.0] },
+        Vertex { position: [0.5, 0.5, -0.5], color: [0.0, 0.0, 1.0] },
+        Vertex { position: [-0.5, 0.5, -0.5], color: [1.0, 1.0, 1.0] },
     ]
 }
 
 fn triangle_geom_indices() -> Vec<GeometryDataIndex> {
-    vec![ 0, 1, 2, 2, 3, 0 ]
+    vec![ 0, 1, 2, 2, 3, 0,
+          4, 5, 6, 6, 7, 4 ]
 }
 
 fn create_and_allocate_buffer(device: &ash::Device, instance: &ash::Instance, physical_device: vk::PhysicalDevice, buffer_create_info: vk::BufferCreateInfo, desired_properties: vk::MemoryPropertyFlags) -> (vk::Buffer, vk::DeviceMemory) {
